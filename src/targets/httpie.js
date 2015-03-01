bf3e22ede4ecb017ebfc91af5c90b0838c20354e
Ahmad Nassri <ahmad@codeinchaos.com>
2015-02-28 20:40:37 -0500

    additional httpie arguments + test

--- src/targets/httpie.js
+++ src/targets/httpie.js
@@ -4,16 +4,65 @@ var util = require('util');
 
 module.exports = function (options) {
   var opts = util._extend({
-    indent: '    '
+    body: false,
+    cert: false,
+    headers: false,
+    indent: '    ',
+    pretty: false,
+    print: false,
+    short: false,
+    style: false,
+    timeout: false,
+    verbose: false,
+    verify: false
   }, options);
 
   var code = [];
 
+  // start with body pipe
   if (this.source.postData) {
     code.push(util.format('echo %s | ', JSON.stringify(this.source.postData.text)));
   }
 
-  code.push(util.format('http %s %s%s', this.source.method, this.source.uriObj.hostname, this.source.uriObj.pathname));
+  var flags = [];
+
+  if (opts.headers) {
+    flags.push(opts.short ? '-h' : '--headers');
+  }
+
+  if (opts.body) {
+    flags.push(opts.short ? '-b' : '--body');
+  }
+
+  if (opts.verbose) {
+    flags.push(opts.short ? '-v' : '--verbose');
+  }
+
+  if (opts.print) {
+    flags.push(util.format('%s=%s', opts.short ? '-p' : '--print', opts.print));
+  }
+
+  if (opts.verify) {
+    flags.push(util.format('--verify=%s', opts.verify));
+  }
+
+  if (opts.cert) {
+    flags.push(util.format('--cert=%s', opts.cert));
+  }
+
+  if (opts.pretty) {
+    flags.push(util.format('--pretty=%s', opts.pretty));
+  }
+
+  if (opts.style) {
+    flags.push(util.format('--style=%s', opts.pretty));
+  }
+
+  if (opts.timeout) {
+    flags.push(util.format('--timeout=%s', opts.timeout));
+  }
+
+  code.push(util.format('http %s%s %s', flags.length ? flags.join(' ') + ' ' : '', this.source.method, this.source.url));
 
   // construct query params
   if (this.source.queryString && this.source.queryString.length) {
@@ -22,16 +71,20 @@ module.exports = function (options) {
     });
   }
 
+  // construct headers
   if (this.source.headers && this.source.headers.length) {
     this.source.headers.map(function (header) {
       code.push(util.format('%s:%s', header.name, header.value));
     });
   }
 
-  if (this.source.headers && this.source.headers.length) {
-    this.source.headers.map(function (header) {
-      code.push(util.format('%s:%s', header.name, header.value));
+  // construct cookies argument
+  if (this.source.cookies && this.source.cookies.length) {
+    var cookies = this.source.cookies.map(function (cookie) {
+      return encodeURIComponent(cookie.name) + '=' + encodeURIComponent(cookie.value);
     });
+
+    code.push(util.format('Cookie:%s', cookies.join('; ')));
   }
 
   return code.join(opts.indent !== false ? ' \\\n' + opts.indent : ' ');
--- test/targets/httpie.js
+++ test/targets/httpie.js
@@ -12,7 +12,32 @@ describe('httpie', function () {
     });
 
     result.should.be.a.String;
-    result.should.eql('echo "{\\"foo\\": \\"bar\\"}" |  http POST httpconsole.com/debug Content-Type:application/json Content-Type:application/json');
+    result.should.eql('echo "{\\"foo\\": \\"bar\\"}" |  http POST http://httpconsole.com/debug Content-Type:application/json Cookie:bar=baz');
+
+    done();
+  });
+
+  it('should ask for verbose output', function (done) {
+    var result = new HTTPSnippet(fixtures.simple).httpie({
+      indent: false,
+      verbose: true
+    });
+
+    result.should.be.a.String;
+    result.should.eql('echo "{\\"foo\\": \\"bar\\"}" |  http --verbose POST http://httpconsole.com/debug Content-Type:application/json Cookie:bar=baz');
+
+    done();
+  });
+
+  it('should add flags', function (done) {
+    var result = new HTTPSnippet(fixtures.short).httpie({
+      indent: false,
+      cert: 'foo',
+      verbose: true
+    });
+
+    result.should.be.a.String;
+    result.should.eql('http --verbose --cert=foo GET http://httpconsole.com/echo');
 
     done();
   });
@@ -23,7 +48,7 @@ describe('httpie', function () {
     });
 
     result.should.be.a.String;
-    result.replace(/\\\n/g, '').should.eql('echo "{\\"foo\\": \\"bar\\"}" |  @http POST httpconsole.com/debug @Content-Type:application/json @Content-Type:application/json');
+    result.replace(/\\\n/g, '').should.eql('echo "{\\"foo\\": \\"bar\\"}" |  @http POST http://httpconsole.com/debug @Content-Type:application/json @Cookie:bar=baz');
 
     done();
   });
