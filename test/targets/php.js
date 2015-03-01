f9ab94d34fcb786b8783d17abf889b14f30a731d
Ahmad Nassri <ahmad@codeinchaos.com>
2015-03-01 03:02:35 -0500

    test full request with PHP

---
+++ test/targets/php.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fixtures = require('../fixtures');
+var HTTPSnippet = require('../../src');
+var should = require('should');
+
+describe('PHP', function () {
+  it('should convert full request to cURL command', function (done) {
+    var result = new HTTPSnippet(fixtures.full).php({
+      indent: '\t'
+    });
+
+    result.replace(/\n/g, '').should.eql('<?php$curl = curl_init();curl_setopt_array($curl, array(\tCURLOPT_URL => "http://httpconsole.com/debug?baz=abc&foo=bar&foo=baz",\tCURLOPT_RETURNTRANSFER => true,\tCURLOPT_ENCODING => "",\tCURLOPT_MAXREDIRS => 10,\tCURLOPT_TIMEOUT => 30,\tCURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,\tCURLOPT_CUSTOMREQUEST => "POST",\tCURLOPT_POSTFIELDS => "{\\"foo\\": \\"bar\\"}",\tCURLOPT_COOKIE => "foo=bar; bar=baz",\tCURLOPT_HTTPHEADER => array(\t\t"Accept: text/plain",\t\t"Content-Type: application/json",\t\t"X-Pretty-Print: 2"\t),));$response = curl_exec($curl);curl_close($curl);');
+
+    done();
+  });
+});
