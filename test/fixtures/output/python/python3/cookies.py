31d9e9dcde2d8151c68e15492a632b0ce5719911
José Padilla <jpadilla@webapplicate.com>
2015-03-19 10:08:38 -0400

    Update cookies.py

--- test/fixtures/output/python/python3/cookies.py
+++ test/fixtures/output/python/python3/cookies.py
@@ -4,7 +4,7 @@ conn = http.client.HTTPConnection("mockbin.com")
 
 headers = { 'cookie': "foo=bar; bar=baz" }
 
-conn.request("POST", "/har", headers = headers)
+conn.request("POST", "/har", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
