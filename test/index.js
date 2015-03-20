a207126ac8993528adfee297abc51c99de80009d
Ahmad Nassri <ahmad@codeinchaos.com>
2015-03-20 04:50:37 -0700

    add more tests

--- test/index.js
+++ test/index.js
@@ -5,11 +5,110 @@
 var fixtures = require('./fixtures')
 var HTTPSnippet = require('../src')
 
-require('should')
+var should = require('should')
 
 describe('HTTPSnippet', function () {
+  it('should return false if no matching target', function (done) {
+    var snippet = new HTTPSnippet(fixtures.requests.short)
+
+    snippet.convert(null).should.eql(false)
+
+    done()
+  })
+
+  it('should fail validation', function (done) {
+    var snippet;
+
+    /*eslint-disable no-wrap-func */
+    (function () {
+      snippet = new HTTPSnippet({yolo: 'foo'})
+    }).should.throw(Array)
+
+    should.not.exist(snippet)
+
+    done()
+  })
+
+  it('should parse HAR file with multiple entries', function (done) {
+    var snippet = new HTTPSnippet(fixtures.har)
+
+    snippet.should.have.property('requests').and.be.an.Array
+    snippet.requests.length.should.equal(2)
+
+    var results = snippet.convert('shell')
+
+    results.should.be.an.Array
+    results.length.should.equal(2)
+
+    done()
+  })
+
+  it('should convert multipart/mixed to multipart/form-data', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['multipart/mixed']).requests[0]
+
+    req.postData.mimeType.should.eql('multipart/form-data')
+
+    done()
+  })
+
+  it('should convert multipart/related to multipart/form-data', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['multipart/related']).requests[0]
+
+    req.postData.mimeType.should.eql('multipart/form-data')
+
+    done()
+  })
+
+  it('should convert multipart/alternative to multipart/form-data', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['multipart/alternative']).requests[0]
+
+    req.postData.mimeType.should.eql('multipart/form-data')
+
+    done()
+  })
+
+  it('should convert text/json to application/json', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['text/json']).requests[0]
+
+    req.postData.mimeType.should.eql('application/json')
+
+    done()
+  })
+
+  it('should convert text/x-json to application/json', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['text/x-json']).requests[0]
+
+    req.postData.mimeType.should.eql('application/json')
+
+    done()
+  })
+
+  it('should convert application/x-json to application/json', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['application/x-json']).requests[0]
+
+    req.postData.mimeType.should.eql('application/json')
+
+    done()
+  })
+
+  it('should gracefully fallback if not able to parse JSON', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['invalid-json']).requests[0]
+
+    req.postData.mimeType.should.eql('text/plain')
+
+    done()
+  })
+
+  it('should set postData.text = empty string when postData.params === undefined in application/x-www-form-urlencoded', function (done) {
+    var req = new HTTPSnippet(fixtures.mimetypes['application/x-www-form-urlencoded']).requests[0]
+
+    req.postData.text.should.eql('')
+
+    done()
+  })
+
   it('should add "uriObj" to source object', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.query).source
+    var req = new HTTPSnippet(fixtures.requests.query).requests[0]
 
     req.uriObj.should.be.an.Object
     req.uriObj.should.eql({
@@ -38,7 +137,7 @@ describe('HTTPSnippet', function () {
   })
 
   it('should add "queryObj" to source object', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.query).source
+    var req = new HTTPSnippet(fixtures.requests.query).requests[0]
 
     req.queryObj.should.be.an.Object
     req.queryObj.should.eql({
@@ -54,7 +153,7 @@ describe('HTTPSnippet', function () {
   })
 
   it('should add "headersObj" to source object', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.headers).source
+    var req = new HTTPSnippet(fixtures.requests.headers).requests[0]
 
     req.headersObj.should.be.an.Object
     req.headersObj.should.eql({
@@ -66,7 +165,7 @@ describe('HTTPSnippet', function () {
   })
 
   it('should modify orignal url to strip query string', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.query).source
+    var req = new HTTPSnippet(fixtures.requests.query).requests[0]
 
     req.url.should.be.a.String
     req.url.should.eql('http://mockbin.com/har')
@@ -75,7 +174,7 @@ describe('HTTPSnippet', function () {
   })
 
   it('should add "fullUrl" to source object', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.query).source
+    var req = new HTTPSnippet(fixtures.requests.query).requests[0]
 
     req.fullUrl.should.be.a.String
     req.fullUrl.should.eql('http://mockbin.com/har?foo=bar&foo=baz&baz=abc&key=value')
@@ -84,7 +183,7 @@ describe('HTTPSnippet', function () {
   })
 
   it('should fix "path" property of "uriObj" to match queryString', function (done) {
-    var req = new HTTPSnippet(fixtures.requests.query).source
+    var req = new HTTPSnippet(fixtures.requests.query).requests[0]
 
     req.uriObj.path.should.be.a.String
     req.uriObj.path.should.eql('/har?foo=bar&foo=baz&baz=abc&key=value')
--- test/tests/shell/httpie.js
+++ test/tests/shell/httpie.js
@@ -15,15 +15,41 @@ module.exports = function (HTTPSnippet, fixtures) {
     result.should.eql('http --verbose GET http://mockbin.com/har')
   })
 
-  it('should add flags', function () {
+  it('should use short flags', function () {
     var result = new HTTPSnippet(fixtures.requests.short).convert('shell', 'httpie', {
-      indent: false,
+      body: true,
       cert: 'foo',
-      verbose: true
+      headers: true,
+      indent: false,
+      pretty: 'x',
+      print: 'x',
+      short: true,
+      style: 'x',
+      timeout: 1,
+      verbose: true,
+      verify: 'x'
     })
 
     result.should.be.a.String
-    result.should.eql('http --verbose --cert=foo GET http://mockbin.com/har')
+    result.should.eql('http -h -b -v -p=x --verify=x --cert=foo --pretty=x --style=x --timeout=1 GET http://mockbin.com/har')
+  })
+
+  it('should use long flags', function () {
+    var result = new HTTPSnippet(fixtures.requests.short).convert('shell', 'httpie', {
+      body: true,
+      cert: 'foo',
+      headers: true,
+      indent: false,
+      pretty: 'x',
+      print: 'x',
+      style: 'x',
+      timeout: 1,
+      verbose: true,
+      verify: 'x'
+    })
+
+    result.should.be.a.String
+    result.should.eql('http --headers --body --verbose --print=x --verify=x --cert=foo --pretty=x --style=x --timeout=1 GET http://mockbin.com/har')
   })
 
   it('should use custom indentation', function () {
@@ -32,7 +58,7 @@ module.exports = function (HTTPSnippet, fixtures) {
     })
 
     result.should.be.a.String
-    result.replace(/\\\n/g, '').should.eql("echo foo=bar |  @http POST 'http://mockbin.com/har?foo=bar&foo=baz&baz=abc&key=value' @accept:application/json @content-type:application/x-www-form-urlencoded @cookie:'foo=bar; bar=baz'")
+    result.replace(/\\\n/g, '').should.eql("http --form POST 'http://mockbin.com/har?foo=bar&foo=baz&baz=abc&key=value' @accept:application/json @content-type:application/x-www-form-urlencoded @cookie:'foo=bar; bar=baz' @foo=bar")
   })
 
   it('should use queryString parameters', function () {
@@ -43,5 +69,26 @@ module.exports = function (HTTPSnippet, fixtures) {
 
     result.should.be.a.String
     result.replace(/\\\n/g, '').should.eql('http GET http://mockbin.com/har foo==bar foo==baz baz==abc key==value')
+  })
+
+  it('should build parameterized output of query string', function () {
+    var result = new HTTPSnippet(fixtures.requests.query).convert('shell', 'httpie', {
+      indent: false,
+      queryParams: true
+    })
+
+    result.should.be.a.String
+    result.replace(/\\\n/g, '').should.eql('http GET http://mockbin.com/har foo==bar foo==baz baz==abc key==value')
+  })
+
+  it('should build parameterized output of post data', function () {
+    var result = new HTTPSnippet(fixtures.requests['application-form-encoded']).convert('shell', 'httpie', {
+      short: true,
+      indent: false,
+      queryParams: true
+    })
+
+    result.should.be.a.String
+    result.replace(/\\\n/g, '').should.eql('http -f POST http://mockbin.com/har content-type:application/x-www-form-urlencoded foo=bar hello=world')
   })
 }
