9c0f0f4f72f336557238caf6691a5831b540aa3d
Ahmad Nassri <ahmad@codeinchaos.com>
2015-03-20 04:49:28 -0700

    update shell fixtures

--- test/fixtures/output/shell/curl/multipart-data.sh
+++ test/fixtures/output/shell/curl/multipart-data.sh
@@ -1,4 +1,4 @@
 curl --request POST \
   --url http://mockbin.com/har \
   --header 'content-type: multipart/form-data; boundary=---011000010111000001101001' \
-  --data '-----011000010111000001101001\r\nContent-Disposition: form-data; name="foo"; filename="hello.txt"\r\nContent-Type: text/plain\r\n\r\nHello World\r\n-----011000010111000001101001--'
+  --form 'foo=Hello World'
--- test/fixtures/output/shell/httpie/application-form-encoded.sh
+++ test/fixtures/output/shell/httpie/application-form-encoded.sh
@@ -1,3 +1,4 @@
-echo 'foo=bar&hello=world' |  \
-  http POST http://mockbin.com/har \
-  content-type:application/x-www-form-urlencoded
+http --form POST http://mockbin.com/har \
+  content-type:application/x-www-form-urlencoded \
+  foo=bar \
+  hello=world
