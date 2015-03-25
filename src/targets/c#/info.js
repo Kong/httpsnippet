410eb73f268f8cdab2f8e1935b504ef90d7288e0
Shashi Ranjan <shashi@mashape.com>
2015-03-25 00:44:48 -0700

    C# module

---
+++ src/targets/c#/info.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  key: 'c#',
+  title: 'C#',
+  extname: '.cs',
+  default: 'restsharp'
+}
---
+++ src/targets/c#/restsharp.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var util = require('util')
+
+var CodeBuilder = require('../../helpers/code-builder')
+
+module.exports = function (source, options) {
+  var opts = util._extend({
+    indent: '  '
+  }, options)
+
+  var code = new CodeBuilder(opts.indent)
+
+  var methods = [ 'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS' ]
+
+  if (methods.indexOf(source.method.toUpperCase()) === -1) {
+    return 'Method not supported'
+  } else {
+    code.push(util.format('var client = new RestClient("%s");', source.fullUrl))
+    code.push(util.format('var request = new RestRequest(Method.%s);', source.method.toUpperCase()))
+
+  }
+
+  // construct headers
+  if (source.headers.length) {
+    source.headers.forEach(function (header) {
+      code.push(util.format('request.AddHeader("%s", "%s");', header.name, header.value))
+    })
+  }
+
+  // construct cookies
+  if (source.cookies.length) {
+    source.cookies.forEach(function (cookie) {
+      code.push(util.format('request.AddCookie("%s", "%s");', cookie.name, cookie.value))
+    })
+  }
+
+  if (source.postData.text) {
+    code.push(util.format('request.AddParameter("%s", %s, ParameterType.RequestBody);', source.allHeaders['content-type'], JSON.stringify(source.postData.text)))
+  }
+
+  code.push('IRestResponse response = client.Execute(request);')
+  return code.join()
+}
+
+module.exports.info = {
+  key: 'restsharp',
+  title: 'RestSharp',
+  link: 'http://restsharp.org/',
+  description: 'Simple REST and HTTP API Client for .NET'
+}
