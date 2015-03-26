64cdb5c58247f32fb3e595c2cd21284027409d09
Shashi Ranjan <shashi@mashape.com>
2015-03-26 03:40:54 -0700

    added spacing

--- src/targets/c/libcurl.js
+++ src/targets/c/libcurl.js
@@ -6,6 +6,8 @@ module.exports = function (source, options) {
   var code = new CodeBuilder()
 
   code.push('CURL *hnd = curl_easy_init();')
+  code.push('')
+
   code.push('curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "%s");', source.method.toUpperCase())
   code.push('curl_easy_setopt(hnd, CURLOPT_URL, "%s");', source.fullUrl)
 
@@ -14,6 +16,7 @@ module.exports = function (source, options) {
 
   // construct headers
   if (headers.length) {
+    code.push('')
     code.push('struct curl_slist *headers = NULL;')
     headers.map(function (key) {
       code.push('headers = curl_slist_append(headers, "%s: %s");', key, source.headersObj[key])
@@ -23,13 +26,16 @@ module.exports = function (source, options) {
 
   // construct cookies
   if (source.allHeaders.cookie) {
+    code.push('')
     code.push('curl_easy_setopt(hnd, CURLOPT_COOKIE, "%s");', source.allHeaders.cookie)
   }
 
   if (source.postData.text) {
+    code.push('')
     code.push('curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, %s);', JSON.stringify(source.postData.text))
   }
 
+  code.push('')
   code.push('CURLcode ret = curl_easy_perform(hnd);')
 
   return code.join()
