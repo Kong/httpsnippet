d962a369728f508a692ad3d684aa2f7eec567ce9
SGrondin <github@simongrondin.name>
2015-04-06 12:38:17 -0700

    Updated OCaml with the new Cohttp 0.16 features

--- test/fixtures/output/ocaml/cohttp/cookies.ml
+++ test/fixtures/output/ocaml/cohttp/cookies.ml
@@ -3,9 +3,7 @@ open Cohttp
 open Lwt
 
 let uri = Uri.of_string "http://mockbin.com/har" in
-let headers = Header.init ()
-  |> fun h -> Header.add h "cookie" "foo=bar; bar=baz"
-in
+let headers = Header.add (Header.init ()) "cookie" "foo=bar; bar=baz" in
 
 Client.call ~headers `POST uri
 >>= fun (res, body_stream) ->
