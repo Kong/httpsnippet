5c3b4745429ac967aaef56a16e6ff1955e660ca1
Pascal Matthiesen <pascal.matthiesen@check24.de>
2017-06-12 11:41:23 +0200

    added fetch implementation

---
+++ test/fixtures/output/javascript/fetch/multipart-form-data.js
@@ -0,0 +1,15 @@
+var form = new FormData();
+form.append("foo", "bar");
+
+fetch('http://mockbin.com/har', {
+  "method": "POST",
+  "headers": {
+    "content-type": "multipart/form-data; boundary=---011000010111000001101001"
+  }
+})
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.log(err);
+});
