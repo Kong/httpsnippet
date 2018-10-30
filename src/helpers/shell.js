501599cb4369fc33ec37cbb6cccef2e41583dc65
Ilija <omniiknight@hotmail.com>
2018-10-30 13:48:56 +0100

    Update shell.js

--- src/helpers/shell.js
+++ src/helpers/shell.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var util = require('util')
+const util = require('util')
 
 module.exports = {
   /**
@@ -8,8 +8,8 @@ module.exports = {
    * to deal with nested single quote characters.
    * http://wiki.bash-hackers.org/syntax/quoting#strong_quoting
    */
-  quote: function (value) {
-    var safe = /^[a-z0-9-_/.@%^=:]+$/i
+  quote(value) {
+    let safe = /^[a-z0-9-_/.@%^=:]+$/i
 
     // Unless `value` is a simple shell-safe string, quote it.
     if (!safe.test(value)) {
@@ -19,7 +19,7 @@ module.exports = {
     return value
   },
 
-  escape: function (value) {
+  escape(value) {
     return value.replace(/\r/g, '\\r').replace(/\n/g, '\\n')
   }
 }
