ca9d23da35562cf1b331f48bc320b22ee83dcc0b
gabrielakoreeda <gabriela.koreeda@gmail.com>
2019-03-30 12:15:26 -0300

    Add r target from httr library

--- src/targets/index.js
+++ src/targets/index.js
@@ -13,6 +13,7 @@ module.exports = {
   php: require('./php'),
   powershell: require('./powershell'),
   python: require('./python'),
+  r: require('./r'),
   ruby: require('./ruby'),
   shell: require('./shell'),
   swift: require('./swift')
