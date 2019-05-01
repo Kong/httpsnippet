480371f49990e44351a26657b37e03103547ab06
Karol Maciaszek <karol@maciaszek.pl>
2019-05-01 11:41:15 +0100

    test(target/python) query params support in HAR url

--- test/targets/python/requests.js
+++ test/targets/python/requests.js
@@ -2,5 +2,13 @@
 
 require('should')
 
-module.exports = function (snippet, fixtures) {
+module.exports = function (HTTPSnippet) {
+  it('should support query parameters provided in HAR\'s url', function () {
+    var result = new HTTPSnippet({ "method": "GET", "url": "http://mockbin.com/har?param=value" }).convert('python', 'requests', {
+      showBoilerplate: false
+    })
+
+    result.should.be.a.String()
+    result.should.eql('import requests\n\nurl = "http://mockbin.com/har"\n\nquerystring = {"param":"value"}\n\nresponse = requests.request("GET", url, params=querystring)\n\nprint(response.text)')
+  })
 }
