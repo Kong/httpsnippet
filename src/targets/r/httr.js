7b10985be746d22a5f769e40844eb5dda0bddbbb
gabrielakoreeda <gabriela.koreeda@gmail.com>
2019-05-07 20:59:27 -0300

    Fix lint errors

--- src/targets/r/httr.js
+++ src/targets/r/httr.js
@@ -11,137 +11,135 @@
 var util = require('util')
 var CodeBuilder = require('../../helpers/code-builder')
 
-module.exports = function(source, options){
- // Start snippet
- var code = new CodeBuilder()
+module.exports = function (source, options) {
+  // Start snippet
+  var code = new CodeBuilder()
 
- // Import httr
- code.push('library(httr)')
-     .blank()
+  // Import httr
+  code.push('library(httr)')
+    .blank()
 
- // Set URL
- code.push('url <- "%s"', source.url)
-     .blank()
+  // Set URL
+  code.push('url <- "%s"', source.url)
+    .blank()
 
- // Construct query string
- var query
- var qs = source.queryObj
- var queryCount = Object.keys(qs).length
- delete source.queryObj['key']
+  // Construct query string
+  var query
+  var qs = source.queryObj
+  var queryCount = Object.keys(qs).length
+  delete source.queryObj['key']
 
- if (source.queryString.length == 1) {
-   code.push('queryString <- list("%s" = "%s")', Object.keys(qs), Object.values(qs))
-       .blank()
- } else if (source.queryString.length > 1) {
-  var count = 1
+  if (source.queryString.length === 1) {
+    code.push('queryString <- list("%s" = "%s")', Object.keys(qs), Object.values(qs))
+      .blank()
+  } else if (source.queryString.length > 1) {
+    var count = 1
 
-  code.push('queryString <- list(')
+    code.push('queryString <- list(')
 
-  for (query in qs) {
-    if (count++ !== queryCount - 1) {
-      code.push('  "%s" = "%s",', query, qs[query])
-    } else {
-      code.push('  "%s" = "%s"', query, qs[query])
+    for (query in qs) {
+      if (count++ !== queryCount - 1) {
+        code.push('  "%s" = "%s",', query, qs[query])
+      } else {
+        code.push('  "%s" = "%s"', query, qs[query])
+      }
+    }
+
+    code.push(')')
+      .blank()
+  }
+
+  // Construct payload
+  var payload = JSON.stringify(source.postData.text)
+
+  if (payload) {
+    code.push('payload <- %s', payload)
+      .blank()
+  }
+
+  // Define encode
+  if (source.postData.text || source.postData.jsonObj || source.postData.params) {
+    switch (source.postData.mimeType) {
+      case 'application/x-www-form-urlencoded':
+        code.push('encode <- "form"')
+          .blank()
+        break
+
+      case 'application/json':
+        code.push('encode <- "json"')
+          .blank()
+        break
+
+      case 'multipart/form-data':
+        code.push('encode <- "multipart"')
+          .blank()
+        break
+
+      default:
+        code.push('encode <- "raw"')
+          .blank()
+        break
     }
   }
 
-  code.push(')')
-      .blank()
+  // Construct headers
+  var head
+  var headers = source.allHeaders
+  var headerCount = Object.keys(headers).length
+  var header
 
- }
+  if (headerCount === 1) {
+    for (head in headers) {
+      header = ', add_headers(' + head.replace('-', '_') + " = '" + headers[head] + "')"
+    }
+  } else if (headerCount > 1) {
+    var countHeader = 1
 
- // Construct payload
- var payload = JSON.stringify(source.postData.text)
+    header = ', add_headers('
 
- if (payload) {
-   code.push('payload <- %s', payload)
-       .blank()
- }
+    for (head in headers) {
+      if (countHeader++ !== headerCount) {
+        header += head.replace('-', '_') + " = '" + headers[head] + "', "
+      } else {
+        header += head.replace('-', '_') + " = '" + headers[head] + "')"
+      }
+    }
+  }
 
- // Define encode
- var encode
- if (source.postData.text || source.postData.jsonObj || source.postData.params){
-   switch (source.postData.mimeType) {
-     case 'application/x-www-form-urlencoded':
-       code.push('encode <- "form"')
-           .blank()
-       break;
+  // Construct request
+  var method = source.method
+  var request = util.format('response <- VERB("%s", url', method)
 
-     case 'application/json':
-       code.push('encode <- "json"')
-           .blank()
-       break;
+  if (payload) {
+    request += ', body = payload'
+  }
 
-     case 'multipart/form-data':
-       code.push('encode <- "multipart"')
-           .blank()
-       break;
+  if (headerCount > 0) {
+    request += header
+  }
 
-     default:
-       code.push('encode <- "raw"')
-           .blank()
-       break;
-   }
- }
+  if (source.queryString.length) {
+    request += ', query = queryString'
+  }
 
- // Construct headers
- var head
- var headers = source.allHeaders
- var headerCount = Object.keys(headers).length
- var header
+  if (source.postData.text || source.postData.jsonObj || source.postData.params) {
+    request += ', encode = encode'
+  }
 
- if (headerCount === 1) {
-     for (head in headers) {
-       header = ", add_headers(" + head.replace("-", "_") + " = '" + headers[head] + "')"
-     }
- } else if (headerCount > 1) {
-   var count = 1
+  request += ')'
 
-   header = ", add_headers("
+  code.push(request)
+    .blank()
 
-   for (head in headers) {
-     if (count++ !== headerCount) {
-       header += head.replace("-", "_") + " = '" + headers[head] + "', "
-     } else {
-       header += head.replace("-", "_") + " = '" + headers[head] + "')"
-     }
-   }
- }
+  // Print response
+    .push('content(response, "text")')
 
- // Construct request
- var method = source.method
- var request = util.format('response <- VERB("%s", url', method)
-
- if (payload) {
-   request += ', body = payload'
- }
-
- if (headerCount > 0) {
-   request += header
- }
-
- if (source.queryString.length) {
-   request += ', query = queryString'
- }
-
- if (source.postData.text || source.postData.jsonObj || source.postData.params) {
-   request += ', encode = encode'
- }
-
- request += ')'
-
- code.push(request)
-     .blank()
-
-     // Print response
-     .push('content(response, "text")')
- 
- return code.join()
+  return code.join()
 }
 
 module.exports.info = {
-   key: 'httr',
-   title: 'httr',
-   link: 'https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html',
-   description: 'R HTTP library'
+  key: 'httr',
+  title: 'httr',
+  link: 'https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html',
+  description: 'R HTTP library'
 }
