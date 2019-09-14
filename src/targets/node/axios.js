3cf4b47de62d5088f57235cabf72af0dc3c81ddb
Sefi Krausz <sefikrausz@ip-192-168-1-10.ec2.internal>
2019-09-14 10:24:01 +0300

    changes to axios target

--- src/targets/node/axios.js
+++ src/targets/node/axios.js
@@ -10,31 +10,78 @@
 
 'use strict'
 
-var CodeBuilder = require('../../helpers/code-builder')
+let CodeBuilder = require('../../helpers/code-builder')
 
 module.exports = function (source, options) {
-  var opts = Object.assign({
+  let opts = Object.assign({
     indent: '  '
   }, options)
 
-  var includeFS = false
-  var code = new CodeBuilder(opts.indent)
+  let includeFS = false
+  let code = new CodeBuilder(opts.indent)
 
   code.push('const axios = require("axios");')
     .blank()
-  // .push('var req = unirest("%s", "%s");', source.method, source.url)
-  // .blank()
 
-  let requestOptions = {
+  const requestOptions = {
     method: `${source.method}`,
     headers: {
       'content-type': `${source.postData.mimeType}`,
       ...(Object.keys(source.headersObj).length && source.headersObj)
     },
-    params: Object.keys(source.queryObj).length && source.queryObj,
-    url: `${source.url}`,
+    params: Object.keys(source.queryObj).length ? source.queryObj : undefined,
+    url: `${source.url}`
   };
 
+  switch (source.postData.mimeType) {
+    case 'application/x-www-form-urlencoded':
+      if (source.postData.paramsObj) {
+        code.push('req.form(%s);', JSON.stringify(source.postData.paramsObj, null, opts.indent))
+      }
+      break
+
+    case 'application/json':
+      if (source.postData.jsonObj) {
+        code.push('req.type("json");')
+          .push('req.send(%s);', JSON.stringify(source.postData.jsonObj, null, opts.indent))
+      }
+      break
+
+    case 'multipart/form-data':
+      var multipart = []
+
+      source.postData.params.forEach(function (param) {
+        var part = {}
+
+        if (param.fileName && !param.value) {
+          includeFS = true
+
+          part.body = 'fs.createReadStream("' + param.fileName + '")'
+        } else if (param.value) {
+          part.body = param.value
+        }
+
+        if (part.body) {
+          if (param.contentType) {
+            part['content-type'] = param.contentType
+          }
+
+          multipart.push(part)
+        }
+      })
+
+      code.push('req.multipart(%s);', JSON.stringify(multipart, null, opts.indent))
+      break
+
+    default:
+      if (source.postData.text) {
+        code.push(opts.indent + 'req.send(%s);', JSON.stringify(source.postData.text, null, opts.indent))
+      }
+  }
+
+  // if (includeFS) {
+  //   code.unshift('var fs = require("fs");')
+  // }
   code.push(`axios(${
     JSON.stringify(requestOptions)
     })
@@ -44,81 +91,6 @@ module.exports = function (source, options) {
     .catch((error)=>{
       console.log(error)
     })`)
-  // .push(`.then((response)=>{
-  //     console.log(response)
-  //   })`)
-  // .push(`.catch((error)=>{
-  //     console.log(error)
-  //   })`)
-
-  //TODO implement params
-  // if (Object.keys(source.queryObj).length) {
-  //   code.push('req.query(%s);', JSON.stringify(source.queryObj, null, opts.indent))
-  //     .blank()
-  // }
-
-  // if (Object.keys(source.headersObj).length) {
-  //   code.push('req.headers(%s);', JSON.stringify(source.headersObj))
-
-  // }
-
-  // switch (source.postData.mimeType) {
-  //   case 'application/x-www-form-urlencoded':
-  //     if (source.postData.paramsObj) {
-  //       code.push('req.form(%s);', JSON.stringify(source.postData.paramsObj, null, opts.indent))
-  //     }
-  //     break
-
-  //   case 'application/json':
-  //     if (source.postData.jsonObj) {
-  //       code.push('req.type("json");')
-  //         .push('req.send(%s);', JSON.stringify(source.postData.jsonObj, null, opts.indent))
-  //     }
-  //     break
-
-  //   case 'multipart/form-data':
-  //     var multipart = []
-
-  //     source.postData.params.forEach(function (param) {
-  //       var part = {}
-
-  //       if (param.fileName && !param.value) {
-  //         includeFS = true
-
-  //         part.body = 'fs.createReadStream("' + param.fileName + '")'
-  //       } else if (param.value) {
-  //         part.body = param.value
-  //       }
-
-  //       if (part.body) {
-  //         if (param.contentType) {
-  //           part['content-type'] = param.contentType
-  //         }
-
-  //         multipart.push(part)
-  //       }
-  //     })
-
-  //     code.push('req.multipart(%s);', JSON.stringify(multipart, null, opts.indent))
-  //     break
-
-  //   default:
-  //     if (source.postData.text) {
-  //       code.push(opts.indent + 'req.send(%s);', JSON.stringify(source.postData.text, null, opts.indent))
-  //     }
-  // }
-
-  // if (includeFS) {
-  //   code.unshift('var fs = require("fs");')
-  // }
-
-  // code.blank()
-  //   .push('req.end(function (res) {')
-  //   .push(1, 'if (res.error) throw new Error(res.error);')
-  //   .blank()
-  //   .push(1, 'console.log(res.body);')
-  //   .push('});')
-  //   .blank()
 
   return code.join().replace(/"fs\.createReadStream\(\\"(.+)\\"\)"/, 'fs.createReadStream("$1")')
 }
