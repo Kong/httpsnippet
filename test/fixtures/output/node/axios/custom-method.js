40cde9dc136be39dc203f306ec80f6a7db97d111
Rohit Gohri <work@rohit.page>
2020-08-12 10:40:50 +0530

    Add tests for axios clients

---
+++ test/fixtures/output/node/axios/custom-method.js
@@ -0,0 +1,9 @@
+var axios = require("axios").default;
+
+var options = {method: 'PROPFIND', url: 'http://mockbin.com/har'};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
