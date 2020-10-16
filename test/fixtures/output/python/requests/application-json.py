39d6773fda989efac4c3a032a9184953e5300a1f
Jon Ursenbach <jon@ursenba.ch>
2020-10-15 17:40:48 -0700

    feat: cleaner python json snippets

--- test/fixtures/output/python/requests/application-json.py
+++ test/fixtures/output/python/requests/application-json.py
@@ -2,9 +2,16 @@ import requests
 
 url = "http://mockbin.com/har"
 
-payload = "{\"number\":1,\"string\":\"f\\\"oo\",\"arr\":[1,2,3],\"nested\":{\"a\":\"b\"},\"arr_mix\":[1,\"a\",{\"arr_mix_nested\":{}}],\"boolean\":false}"
-headers = {'content-type': 'application/json'}
+payload = {
+    "number": 1,
+    "string": "f\"oo",
+    "arr": [1, 2, 3],
+    "nested": {"a": "b"},
+    "arr_mix": [1, "a", {"arr_mix_nested": {}}],
+    "boolean": False
+}
+headers = {"content-type": "application/json"}
 
-response = requests.request("POST", url, data=payload, headers=headers)
+response = requests.request("POST", url, json=payload, headers=headers)
 
 print(response.text)
