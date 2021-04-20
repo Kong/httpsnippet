6f92ca2f93f91317557408233f92ceff5784b052
Roberto Arruda <robertoadearruda@gmail.com>
2021-04-20 20:55:42 -0300

    Update full.php

--- test/fixtures/output/php/guzzle/full.php
+++ test/fixtures/output/php/guzzle/full.php
@@ -3,9 +3,9 @@
 $client = new \GuzzleHttp\Client();
 
 $response = $client->request('POST', 'http://mockbin.com/har?foo=bar&foo=baz&baz=abc&key=value', [
-    'form_params' => array(
-    'foo' => 'bar'
-),
+    'form_params' => [
+      'foo' => 'bar'
+    ],
     'headers' => [
         'accept' => 'application/json',
         'content-type' => 'application/x-www-form-urlencoded',
