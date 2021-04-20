73d87a39784c889ccf38d57198af2d07c174225b
Roberto Arruda <robertoadearruda@gmail.com>
2021-04-20 20:56:32 -0300

    Update multipart-file.php

--- test/fixtures/output/php/guzzle/multipart-file.php
+++ test/fixtures/output/php/guzzle/multipart-file.php
@@ -3,14 +3,14 @@
 $client = new \GuzzleHttp\Client();
 
 $response = $client->request('POST', 'http://mockbin.com/har', [
-    'multipart' => array(
-        array(
+    'multipart' => [
+        [
             'name' => 'foo',
             'type' => 'text/plain',
             'file' => 'test/fixtures/files/hello.txt',
             'data' => null,
-        ),
-    ),
+        ],
+    ],
 ]);
 
 echo $response->getBody();
