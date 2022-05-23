c37a6a103dd99c3f5ecd20ef9c9a1abad9285cbe
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2022-05-23 18:16:26 -0400

    updates (new since this PR started) guzzle fixture

--- src/targets/php/guzzle/fixtures/headers.php
+++ src/targets/php/guzzle/fixtures/headers.php
@@ -5,6 +5,7 @@ $client = new \GuzzleHttp\Client();
 $response = $client->request('GET', 'http://mockbin.com/har', [
   'headers' => [
     'accept' => 'application/json',
+    'x-bar' => 'Foo',
     'x-foo' => 'Bar',
   ],
 ]);
