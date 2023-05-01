9e044b4e5fb7ae86f9623e08cdb5d6ccef9d8332
Jon Ursenbach <jon@ursenba.ch>
2023-05-01 09:28:07 -0700

    chore(deps-dev): bumping to typescript 5

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 /** @type { import('@jest/types').Config.InitialOptions } */
 module.exports = {
   collectCoverage: false,
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
   resetMocks: true,
@@ -13,5 +8,4 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: ['.+\\.test\\.tsx?$'],
   transform: { '^.+\\.tsx?$': 'ts-jest' },
-  // verbose: true,
 };
