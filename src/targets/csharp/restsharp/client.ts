3e9954de82393a4db891784d5ffe916ab7a9a854
Bill Mill <bill@billmill.org>
2023-06-16 11:05:15 -0400

    first cut at supporting restsharp 107+

--- src/targets/csharp/restsharp/client.ts
+++ src/targets/csharp/restsharp/client.ts
@@ -3,6 +3,10 @@ import { escapeForDoubleQuotes } from '../../../helpers/escape';
 import { getHeader } from '../../../helpers/headers';
 import { Client } from '../../targets';
 
+function title(s: string): string {
+  return s[0].toUpperCase() + s.slice(1).toLowerCase();
+}
+
 export const restsharp: Client = {
   info: {
     key: 'restsharp',
@@ -10,7 +14,7 @@ export const restsharp: Client = {
     link: 'http://restsharp.org/',
     description: 'Simple REST and HTTP API Client for .NET',
   },
-  convert: ({ allHeaders, method, fullUrl, headersObj, cookies, postData }) => {
+  convert: ({ allHeaders, method, fullUrl, headersObj, cookies, postData, uriObj }) => {
     const { push, join } = new CodeBuilder();
     const isSupportedMethod = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'].includes(
       method.toUpperCase(),
@@ -20,26 +24,32 @@ export const restsharp: Client = {
       return 'Method not supported';
     }
 
-    push(`var client = new RestClient("${fullUrl}");`);
-    push(`var request = new RestRequest(Method.${method.toUpperCase()});`);
+    push(`var options = new RestClientOptions("${fullUrl}");`);
+    push(`var client = new RestClient(options);`);
+
+    // The first argument is the sub-path to the base URL, given as the
+    // constructor to RestClient; for our purposes we're just giving the entire
+    // URL as the base path so it can be an empty string
+    push(`var request = new RestRequest("");`);
 
     // Add headers, including the cookies
-
     Object.keys(headersObj).forEach(key => {
       push(`request.AddHeader("${key}", "${escapeForDoubleQuotes(headersObj[key])}");`);
     });
 
     cookies.forEach(({ name, value }) => {
-      push(`request.AddCookie("${name}", "${value}");`);
+      push(`request.AddCookie("${name}", "${value}", "${uriObj.pathname}", "${uriObj.host}");`);
     });
 
+    // here we're just assuming that the text is a JSON post, and that content
+    // type is application/json. Improvements welcome to support multipart or
+    // form-urlencoded
     if (postData.text) {
-      const header = getHeader(allHeaders, 'content-type');
       const text = JSON.stringify(postData.text);
-      push(`request.AddParameter("${header}", ${text}, ParameterType.RequestBody);`);
+      push(`request.AddJsonBody(${text}, false);`);
     }
 
-    push('IRestResponse response = client.Execute(request);');
+    push(`var response = await client.${title(method)}Async(request);`);
     return join();
   },
 };
